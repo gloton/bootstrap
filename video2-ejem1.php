90c2c1193b14036561b6cf433df1eb8d8e930979
Gloton <gloton@gmail.com>
2013-05-14 10:23:58 -0400

    Video 2 - Agregando ejemplos Repasando el curso desde el principio, decidi, agregarle los ejemplos del video 2

---
+++ video2-ejem1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>Mi primera pagina en Bootstrap</title>
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.css" />
+	<script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
+</head>
+<body>
+	<h1>Hola mundo</h1>
+	<button class="btn btn-danger" type="submit">Button</button>
+	
+</body>
--- video2-ejem2.php
+++ video2-ejem2.php
@@ -1,5 +1,6 @@
+</html>
 <!DOCTYPE HTML>
-<html lang="en-US">
+<html lang="es">
 <head>
 	<meta charset="UTF-8">
 	<title>Mi primera pagina en Bootstrap</title>
@@ -7,6 +8,16 @@
 	<script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
 </head>
 <body>
+	<div class="container">
+		<div class="row">
+			<div class="span3">
+				Hola lateral
+			</div>
+			<div class="span9">
+				Hola centro
+			</div>
+		</div>
+	</div>
 	<h1>Hola mundo</h1>
 	<button class="btn btn-danger" type="submit">Button</button>
 	
